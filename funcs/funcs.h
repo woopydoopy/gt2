c914f4615553bf5d7f75dab69f71cbb404103dbd
Anna Seletskaya <seletskaya.anna.stu@mail.ru>
2024-03-21 15:53:38 +0300

    прототип программы

---
+++ funcs/funcs.h
@@ -0,0 +1,2 @@
+#define __FUNCS_H__
+#endif // __FUNCS_H__
--- funcs/main.cpp
+++ funcs/main.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
+#include "funcs.h"
 
 using namespace std;
-
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
 }
-
